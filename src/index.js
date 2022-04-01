3c7d3bc9cfbe2f0a64d3a79a9af701bc378bedf0
Alexander Sullivan <alexander.j.sullivan@hotmail.com>
2022-04-01 18:06:02 -0400

    Sentry added for bug tracing

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 import "react-app-polyfill/ie11"; // For IE 11 support
 import "react-app-polyfill/stable"; // For IE 11 support
+// React
 import React from "react";
 import ReactDOM from "react-dom";
+// Sentry
+import * as Sentry from "@sentry/react";
+import {BrowserTracing} from "@sentry/tracing";
+// Main app
 import "./index.css";
 import App from "./scripts/App";
+// Service workers
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+// Google Analytics
 import reportWebVitals from "./reportWebVitals";
+// Firebase
 // eslint-disable-next-line no-unused-vars
 import * as firebase from "./firebase";
+
+Sentry.init({
+	dsn: process.env.REACT_APP_SENTRY,
+	integrations: [new BrowserTracing()],
+
+	// Set tracesSampleRate to 1.0 to capture 100%
+	// of transactions for performance monitoring.
+	// We recommend adjusting this value in production
+	tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
