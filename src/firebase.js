9e61d36b91f65b5f2c6c590bfd0bfcdbdb62a523
Alexander Sullivan <alexander.j.sullivan@hotmail.com>
2022-04-25 18:53:14 -0400

    Made SVGs smaller
    Optimizations:
    • Added more Twitter meta-data
    • Made SVGs smaller
    
    Update:
    • Updated Material-UI packages
    
    Documentation:
    • Prettified some code

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,6 @@
 import {initializeApp} from "firebase/app";
 import {getAnalytics} from "firebase/analytics";
 import {getPerformance} from "firebase/performance";
-// TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
