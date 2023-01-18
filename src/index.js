760a48826dbc02aff0f110a01290f083391e8f5f
Alexander Sullivan <alexander.j.sullivan@hotmail.com>
2023-01-18 11:19:43 -0500

    Sentry reply support added

--- src/index.js
+++ src/index.js
@@ -15,6 +15,14 @@ import * as firebase from "./firebase";
 
 Sentry.init({
 	dsn: process.env.REACT_APP_SENTRY,
+
+	// This sets the sample rate to be 10%. You may want this to be 100% while
+	// in development and sample at a lower rate in production
+	replaysSessionSampleRate: 0.1,
+	// If the entire session is not sampled, use the below sample rate to sample
+	// sessions when an error occurs.
+	replaysOnErrorSampleRate: 1.0,
+
 	integrations: [
 		new BrowserTracing(),
 		new CaptureConsole({
