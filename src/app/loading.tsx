e222d9a7b09d937817c6192fddecb75c011bdc38
Alexander Sullivan <alexander.j.sullivan@hotmail.com>
2023-12-29 18:36:18 -0500

    Update paths

---
+++ src/app/loading.tsx
@@ -0,0 +1,20 @@
+import { Box, CircularProgress } from "@mui/material";
+
+export default function Loading() {
+	return (
+		<Box
+			sx={{
+				alignItems: 'center',
+				display: 'flex',
+				flexDirection: 'column',
+				flexGrow: 1,
+				height: '100%',
+				justifyContent: 'center',
+				position: 'absolute',
+				width: '100%',
+			}}
+		>
+			<CircularProgress />
+		</Box>
+	);
+}
