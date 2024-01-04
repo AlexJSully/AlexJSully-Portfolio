bf1f417fb00c8b5b126fd1fd68ee9b6b737167c5
Alexander Sullivan <alexander.j.sullivan@hotmail.com>
2024-01-04 10:39:54 -0500

    Navbar fades in

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { HomeRounded as HomeRoundedIcon } from '@mui/icons-material';
-import { AppBar, IconButton, Toolbar, Tooltip, Typography, useMediaQuery, useTheme } from '@mui/material';
+import { AppBar, Fade, IconButton, Toolbar, Tooltip, Typography, useMediaQuery, useTheme } from '@mui/material';
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
@@ -37,64 +37,47 @@ export default function Navbar() {
 		<AppBar
 			sx={{
 				backgroundColor: '#131518',
+				transition: 'all 0.5s ease-in-out',
 			}}
 		>
-			<Toolbar
-				sx={{
-					fontSize: '1.25rem',
-					height: '2rem',
-					justifyContent: 'space-between',
-					zIndex: 10,
-				}}
-			>
-				{/* Home button */}
-				<Link
-					href='/'
-					onClick={(e) => {
-						if (pathname === '/') {
-							e.preventDefault();
-							document.getElementById('content')?.scrollIntoView({ behavior: 'smooth' });
-						}
+			<Fade in timeout={500}>
+				<Toolbar
+					sx={{
+						fontSize: '1.25rem',
+						height: '2rem',
+						justifyContent: 'space-between',
+						zIndex: 10,
+						transition: 'all 0.5s ease-in-out',
 					}}
 				>
-					<Tooltip arrow describeChild title='Home'>
-						<IconButton
-							sx={{
-								color: 'white',
-							}}
-						>
-							<HomeRoundedIcon />
-						</IconButton>
-					</Tooltip>
-				</Link>
-
-				{/* Projects */}
-				<Link
-					href='/#projects-grid'
-					onClick={(e) => {
-						if (pathname === '/') {
-							e.preventDefault();
-							document.getElementById('projects-grid')?.scrollIntoView({ behavior: 'smooth' });
-						}
-					}}
-					style={{
-						color: 'inherit',
-						textDecoration: 'none',
-					}}
-				>
-					<Tooltip arrow describeChild title='View projects'>
-						<Typography>Projects</Typography>
-					</Tooltip>
-				</Link>
-
-				{/* Publications */}
-				{!smallScreen && (
+					{/* Home button */}
 					<Link
-						href='/#publications'
+						href='/'
 						onClick={(e) => {
 							if (pathname === '/') {
 								e.preventDefault();
-								document.getElementById('publications')?.scrollIntoView({ behavior: 'smooth' });
+								document.getElementById('content')?.scrollIntoView({ behavior: 'smooth' });
+							}
+						}}
+					>
+						<Tooltip arrow describeChild title='Home'>
+							<IconButton
+								sx={{
+									color: 'white',
+								}}
+							>
+								<HomeRoundedIcon />
+							</IconButton>
+						</Tooltip>
+					</Link>
+
+					{/* Projects */}
+					<Link
+						href='/#projects-grid'
+						onClick={(e) => {
+							if (pathname === '/') {
+								e.preventDefault();
+								document.getElementById('projects-grid')?.scrollIntoView({ behavior: 'smooth' });
 							}
 						}}
 						style={{
@@ -102,29 +85,50 @@ export default function Navbar() {
 							textDecoration: 'none',
 						}}
 					>
-						<Tooltip arrow describeChild title='View publications'>
-							<Typography>Publications</Typography>
+						<Tooltip arrow describeChild title='View projects'>
+							<Typography>Projects</Typography>
 						</Tooltip>
 					</Link>
-				)}
 
-				{/* Title */}
-				<Typography>
-					<Image
-						alt='Logo'
-						height={24}
-						src={navbarHeaderUse.logo}
-						style={{
-							borderRadius: '50%',
-							filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))',
-							marginRight: '1rem',
-							verticalAlign: 'middle',
-						}}
-						width={24}
-					/>
-					{navbarHeaderUse.text}
-				</Typography>
-			</Toolbar>
+					{/* Publications */}
+					{!smallScreen && (
+						<Link
+							href='/#publications'
+							onClick={(e) => {
+								if (pathname === '/') {
+									e.preventDefault();
+									document.getElementById('publications')?.scrollIntoView({ behavior: 'smooth' });
+								}
+							}}
+							style={{
+								color: 'inherit',
+								textDecoration: 'none',
+							}}
+						>
+							<Tooltip arrow describeChild title='View publications'>
+								<Typography>Publications</Typography>
+							</Tooltip>
+						</Link>
+					)}
+
+					{/* Title */}
+					<Typography>
+						<Image
+							alt='Logo'
+							height={24}
+							src={navbarHeaderUse.logo}
+							style={{
+								borderRadius: '50%',
+								filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))',
+								marginRight: '1rem',
+								verticalAlign: 'middle',
+							}}
+							width={24}
+						/>
+						{navbarHeaderUse.text}
+					</Typography>
+				</Toolbar>
+			</Fade>
 		</AppBar>
 	);
 }
