2e67eede24d5839c3fc5b436a306a90228af0c46
Alexander Sullivan <alexander.j.sullivan@hotmail.com>
2025-02-09 19:55:24 -0500

    just remove cypress test

--- cypress/e2e/landing.cy.ts
+++ cypress/e2e/landing.cy.ts
@@ -11,19 +11,4 @@ describe('Landing Page', () => {
 		// Check that the profile picture exists on the page
 		cy.get('[data-testid="profile_pic"]').should('exist');
 	});
-
-	// This test checks if the YouTube video plays on hover
-	it('should play YouTube video on hover for Masterpiece X project', () => {
-		// Check if the page contains the "Masterpiece X" project and mouse hover over it
-		cy.get('[data-testid="project-mpx-grid"]').should('exist').scrollIntoView().trigger('mouseover');
-
-		// Check if the thumbnail is hidden
-		cy.get('[data-testid="project-mpx-thumbnail"]').should('not.be.visible');
-
-		// Mouse hover out of the project and check if the video stops playing
-		cy.get('[data-testid="project-mpx-grid"]').trigger('mouseout');
-
-		// Check if the video is hidden
-		cy.get('[data-testid="project-mpx-thumbnail"]').should('be.visible');
-	});
 });
